6c05b18fb2317b1bbd11c5a841d030cf1467a8df
ArmSan <mramrasansut@outlook.co.th>
2022-04-08 12:21:56 +0700

    ติดข้อ 1.12 อยู่ รอแก้

---
+++ day2_javascript/1.12.js
@@ -0,0 +1,19 @@
+let text = '';
+function draw(n) {
+    for (i=0;i<n; i++){
+        for (j=0;j<n; j++){
+            if (i==j){
+                text += '*';
+            } else {
+                text += '_';
+            }
+            
+        }
+        text += '\n';
+        
+    }
+    console.log(text);
+}
+draw(2);
+draw(3);
+draw(4);
