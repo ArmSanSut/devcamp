974aac33271188735d297c164afc7081f7272c65
ArmSan <mramrasansut@outlook.co.th>
2022-04-09 12:50:10 +0700

    2.10 done

--- day2_javascript/2.10.js
+++ day2_javascript/2.10.js
@@ -1,6 +1,6 @@
 function draw(n){
     let text = '';
-    for(i=1; i<=n; i++){
+    for(i=n; i>=1; i--){
         for(j=1; j<=(2*n)-1; j++){
             if(j>=n-(i-1) && j<=n+(i-1)){
                 text += '*';
