17df4438ccd40adf37ad1f8ada64dd7192676e3a
ArmSan <mramrasansut@outlook.co.th>
2022-04-11 14:59:52 +0700

    1.12 completed

--- day2_javascript/1.12.js
+++ day2_javascript/1.12.js
@@ -1,15 +1,16 @@
 let text = '';
 function draw(n) {
-    for (i=0;i<n; i++){
-        for (j=0;j<n; j++){
-            if (i==j){
-                text += '*';
-            } else {
+    for (i=n-1; i>=0; i--){
+        for (j=0; j<n; j++){
+            if(i==j) {
                 text += '_';
+            } else {
+                text += '*';
             }
             
         }
-        text += '\n';
+       
+       text += '\n';
         
     }
     console.log(text);
