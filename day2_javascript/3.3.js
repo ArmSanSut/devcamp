41dc7d8681390da076f9abca901ea1de6bad1c00
ArmSan <mramrasansut@outlook.co.th>
2022-04-17 13:43:18 +0700

    3.3 incomplete

---
+++ day2_javascript/3.3.js
@@ -0,0 +1,14 @@
+let pattern = new Array(2);
+function draw(n){
+    for (let i=0; i<pattern.length; i++){
+        pattern[i] = new Array(n);
+    }
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<n; j++) {
+            if(i==j || i>=j){
+                
+            }
+        }
+}
+
+}
