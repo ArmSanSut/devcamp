6a56393da4df46919a91d69b469132a3f9ae828c
ArmSan <mramrasansut@outlook.co.th>
2022-05-27 15:28:14 +0700

    lab commit

---
+++ day24/lab-day24/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+import useStateObj from './useObject';
+
+function App() {
+  let [value, setValue] = useStateObj({ 
+    firstname: 'Steve', 
+    lastname: 'Roger', 
+    age: 50 })
+
+  const onClickFunc = () => {
+    setValue({ age: 2000})
+  }
+  return (
+  <>
+    <h1>{JSON.stringify(value)}</h1>
+    <input type='button' value='Click' onClick={onClickFunc} />
+  </>
+  );
+}
+
+
+
+
+export default App;
