ba04287cd6b3b4040a2fc8cf473f5d2513155b83
ArmSan <mramrasansut@outlook.co.th>
2022-06-10 15:36:15 +0700

    day 16

--- day16/homework-2-backend/routes/users.js
+++ day16/homework-2-backend/routes/users.js
@@ -8,11 +8,11 @@ router.get('/all', async(req, res, next) => {
     host: 'localhost',
     user: 'root', // <== ระบุให้ถูกต้อง
     // password: 'root', // <== ระบุให้ถูกต้อง
-    database: 'codecamp', // <== ระบุ database ให้ถูกต้อง
+    database: 'finalexam', // <== ระบุ database ให้ถูกต้อง
     port: 3306, // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
 });
     try {
-      const users = await connection.query('SELECT * from population');
+      const users = await connection.query('SELECT employee.id as Employee_ID, employee.firstname as fisrtname, employee.lastname as lastname, job.job_name as JOBNAME FROM `employee` INNER JOIN job ON employee.job_id = job.id GROUP BY job.id');
       
       res.json({
         users: users[0]
